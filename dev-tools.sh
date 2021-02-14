c9f8ce720e6dc2f72a0f5fe81d474146d504dd1f
abdelouahedd <ennouriabdelouahed@gmail.com>
2021-02-14 02:07:31 +0100

    dev tools (postman,vscode,intellij,mysql clien)"

---
+++ dev-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Instalation of all tools of dev : using snap package --> "
+
+#install intellij ultimate
+sudo snap install intellij-idea-ultimate --classic
+#install vscode
+sudo snap install code --classic
+#postMan
+sudo snap install postman
+#mysql client
+sudo apt install mysql-client-core-8.0
