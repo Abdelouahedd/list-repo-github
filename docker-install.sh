27d59a8ea39e6711c0edd609885dd9ec2e45fe39
abdelouahedd <ennouriabdelouahed@gmail.com>
2021-02-14 01:36:06 +0100

    change script to install docker

--- docker-install.sh
+++ docker-install.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-set -e
-
-# Install curl
-sudo apt install  curl
 
 # Install docker engine
-echo "Install docker engine"
-curl -fsSl http://get.docker.com -o /tmp/get-docker.sh
-sudo sh /tmp/get-docker.sh
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
 
 # Install docker-compose
-echo "Install docker-compose"
 sudo apt install  docker-compose
 
 # add user to docker group
-echo "add user to docker group"
 user=$(whoami)
 sudo usermod -aG docker "$user"
 
