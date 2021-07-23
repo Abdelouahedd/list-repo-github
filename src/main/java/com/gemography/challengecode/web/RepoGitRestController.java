18e3d1c3cc06105c25acfd3bd896e57a9ee0c364
abdelouahedd <ennouriabdelouahed@gmail.com>
2021-07-23 15:23:53 +0100

    ajouter rest controller avec une seul end point /api/repositorie

---
+++ src/main/java/com/gemography/challengecode/web/RepoGitRestController.java
@@ -0,0 +1,22 @@
+package com.gemography.challengecode.web;
+
+import com.gemography.challengecode.service.RepositorieService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class RepoGitRestController {
+    private RepositorieService repositorieService;
+
+    @GetMapping("/repositorie")
+    public ResponseEntity<?> getNbrRepoUseSameLanguage() throws IOException {
+        return ResponseEntity.ok(this.repositorieService.getNbrRepoUseSameLanguage());
+    }
+}
