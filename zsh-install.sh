c1f0eda87239f08792c0f311f11e3a4425c219af
Abdelouahedd <ennouriabdelouahed@gmail.com>
2021-08-14 19:55:34 +0100

    add oh my zsh install

--- zsh-install.sh
+++ zsh-install.sh
@@ -7,6 +7,8 @@ sudo apt install  curl
 echo "Install zsh :"
 sudo apt install zsh
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "set zsh as default sheel:"
 chsh "$(command -v zsh)"
 
